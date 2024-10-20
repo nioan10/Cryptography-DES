a2475a8f956caf4328f21ea728b653858cfa4d7a
nioan7 <al.alekseev20@yandex.ru>
2024-10-20 23:27:11 +0300

    Smol fix

--- avalanche_key.py
+++ avalanche_key.py
@@ -1,6 +1,7 @@
 import coding
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Функция для изменения одного бита в бинарной строке
@@ -30,56 +31,63 @@ def analyze_avalanche_effect_key(text, key, bit_position):
     # Разбиваем текст на блоки по 64 бита
     blocks = [text_binary[i:i + 64] for i in range(0, len(text_binary), 64)]
 
-    # Изменяем указанный бит в ключе
+        # Изменяем указанный бит в ключе
     modified_key_binary = flip_bit(key_binary, bit_position)
 
     # Генерация ключей для обоих ключей
     round_keys_original = coding.generate_keys(key_binary)
     round_keys_modified = coding.generate_keys(modified_key_binary)
 
+    # Выполним начальную перестановку для текста
+    initial_text_perm = coding.IP(text_binary)
+    
+    # Левые и правые части для сообщения
+    original_left, original_right = initial_text_perm[:32], initial_text_perm[32:]
+    modified_left, modified_right = original_left, original_right
+
+    # Инициализация матрицы отслеживания изменений битов
+    n = 16  # Количество раундов
+    m = 64  # Количество битов (32 бита в левой части + 32 бита в правой части)
+    a_ij_matrix = np.zeros((n, m), dtype=int)  # Матрица a_{ij}, изначально заполнена нулями
+
     # Таблица для сравнения битов на каждом раунде
     comparison_table = []
 
-    # Шифруем каждый блок по отдельности
-    for block_number, block in enumerate(blocks):
-        # Выполним начальную перестановку для блока
-        original_left, original_right = coding.IP(block)[:32], coding.IP(block)[32:]
-        modified_left, modified_right = original_left, original_right
+    # Основной процесс раундов
+    for round_number in range(16):
+        # Сохраняем текущее состояние битов перед раундом
+        previous_left = original_left
+        previous_right = original_right
+        modified_previous_left = modified_left
+        modified_previous_right = modified_right
 
-        # Основной процесс раундов для данного блока
-        for round_number in range(16):
-            # Сохраняем текущее состояние битов перед раундом
-            previous_left = original_left
-            previous_right = original_right
-            modified_previous_left = modified_left
-            modified_previous_right = modified_right
+        # Выполняем раунд шифрования для оригинального ключа и измененного ключа
+        original_left, original_right = coding.round(original_left, original_right, round_keys_original[round_number])
+        modified_left, modified_right = coding.round(modified_left, modified_right, round_keys_modified[round_number])
 
-            # Выполняем раунд шифрования для оригинального ключа и измененного ключа
-            original_left, original_right = coding.round(original_left, original_right, round_keys_original[round_number])
-            modified_left, modified_right = coding.round(modified_left, modified_right, round_keys_modified[round_number])
+        # Сравниваем биты для каждого раунда и обновляем матрицу a_{ij}
+        bit_diff_left = sum(1 for i in range(32) if original_left[i] != modified_left[i])
+        bit_diff_right = sum(1 for i in range(32) if original_right[i] != modified_right[i])
 
-            # Сравниваем биты для каждого раунда
-            bit_diff_left = sum(1 for i in range(32) if original_left[i] != modified_left[i])
-            bit_diff_right = sum(1 for i in range(32) if original_right[i] != modified_right[i])
+        # Заполняем матрицу a_ij для текущего раунда
+        for i in range(32):
+            a_ij_matrix[round_number, i] = 1 if original_left[i] != modified_left[i] else 0
+            a_ij_matrix[round_number, i + 32] = 1 if original_right[i] != modified_right[i] else 0
 
-            # Если этот раунд уже записан в таблицу, добавляем отличия битов
-            if len(comparison_table) > round_number:
-                comparison_table[round_number]['Отличия в левой части (блок {}):'.format(block_number + 1)] = bit_diff_left
-                comparison_table[round_number]['Отличия в правой части (блок {}):'.format(block_number + 1)] = bit_diff_right
-                comparison_table[round_number]['Всего отличий'] += (bit_diff_left + bit_diff_right)
-            else:
-                # Иначе создаем новую запись для этого раунда
-                comparison_table.append({
-                    'Раунд': round_number + 1,
-                    'Отличия в левой части (блок {}):'.format(block_number + 1): bit_diff_left,
-                    'Отличия в правой части (блок {}):'.format(block_number + 1): bit_diff_right,
-                    'Всего отличий': bit_diff_left + bit_diff_right
-                })
+        # Записываем результаты сравнения битов
+        comparison_table.append({
+            'Раунд': round_number + 1,
+            'Отличия в левой части (биты)': bit_diff_left,
+            'Отличия в правой части (биты)': bit_diff_right,
+            'Всего отличий': bit_diff_left + bit_diff_right
+        })
 
     # Создаем таблицу с результатами
     comparison_df = pd.DataFrame(comparison_table)
-    print(comparison_df)
-    return comparison_df
+
+    return comparison_df, a_ij_matrix
+
+
 
 # Пример использования
 #text = "EXAMPLEEEXAMPLEE"  # Сообщение длиной 128 бит
